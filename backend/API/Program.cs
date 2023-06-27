95fce91e0b5039f0df648c8ae7a54762527d14bb
andriy22 <istep.andriy@gmail.com>
2023-06-27 23:41:52 +0300

    added authorization for swagger

--- backend/API/Program.cs
+++ backend/API/Program.cs
@@ -4,6 +4,7 @@ using Application;
 using Application.Common.Mappings;
 using Application.Interfaces;
 using FluentValidation.AspNetCore;
+using Microsoft.OpenApi.Models;
 using Persistence;
 using Serilog;
 using Serilog.Events;
@@ -22,7 +23,34 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "CrowMessenger Open API", Version = "v1" });
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
 
 builder.Services.AddFluentValidationAutoValidation();
 builder.Services.AddFluentValidationClientsideAdapters();
